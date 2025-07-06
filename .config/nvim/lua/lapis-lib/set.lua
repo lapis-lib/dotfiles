8463ac2f71ca43241ede03a1ffb505ccd5304436
lapis-lib <lapis-lib@proton.me>
2025-07-06 16:46:11 +1000

    nvim update

--- .config/nvim/lua/lapis-lib/set.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd('ColorScheme', {
-  pattern = '*',
-  callback = function()
-    -- Define own colours
-    vim.api.nvim_set_hl(0, '@org.agenda.scheduled', { fg = '#ece1d7' })
-  end,
-})
